b4ef785387283a8f5a7336c20660bf6b189d0019
Morariu Catalin <catalin.morariu@metrosystems.net>
2020-01-16 12:05:17 +0200

    Chrome-headless

--- src/main/java/net/metrosystems/mrc/env/DriverUtil.java
+++ src/main/java/net/metrosystems/mrc/env/DriverUtil.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.nio.file.Paths;
+
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
