3f2f50d51cfaaf5882b34e159c2e0a72971cc01c
Morariu Catalin <catalin.morariu@metrosystems.net>
2020-09-15 15:35:07 +0300

    Added run class

---
+++ java/net/metrosystems/tests/stepdefintions/RunMRCTest.java
@@ -0,0 +1,16 @@
+package net.metrosystems.tests.stepdefintions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = {"pretty", "html:target/cucumberHtmlReport"},
+    //pretty:target/cucumber-json-report.json
+    features = "classpath:features",
+    glue = {"net.metrosystems.mrc.seleniumcucumber.stepdefinitions"}
+)
+
+public class RunMRCTest {
+}
