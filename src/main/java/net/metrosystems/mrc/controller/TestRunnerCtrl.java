fe11f070e0f926c5b9d19b96c0d890b16a703f54
Morariu Catalin <catalin.morariu@metrosystems.net>
2020-11-02 21:27:59 +0200

    Import fix

--- src/main/java/net/metrosystems/mrc/controller/TestRunnerCtrl.java
+++ src/main/java/net/metrosystems/mrc/controller/TestRunnerCtrl.java
@@ -1,6 +1,8 @@
 package net.metrosystems.mrc.controller;
 
-import io.cucumber.core.cli.Main;
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Date;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
+
+import io.cucumber.core.cli.Main;
 
 @RestController
 public class TestRunnerCtrl {
