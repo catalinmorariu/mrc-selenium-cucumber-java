b4e53102273f4c6571d4296c712f8234c6df1328
snuyanzin <snuyanzin@gmail.com>
2020-12-16 23:47:36 +0100

    add chrome options

--- src/main/java/net/metrosystems/mrc/CommandLineHeadlessRunner.java
+++ src/main/java/net/metrosystems/mrc/CommandLineHeadlessRunner.java
@@ -3,6 +3,8 @@ package net.metrosystems.mrc;
 import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import io.cucumber.core.cli.Main;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public final class CommandLineHeadlessRunner {
     private CommandLineHeadlessRunner() {
@@ -12,6 +14,18 @@ public final class CommandLineHeadlessRunner {
         System.setProperty("selenide.headless", "true");
         Configuration.headless = true;
         Configuration.browser = Browsers.CHROME;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "http://" + System.getenv("DRP_CF_KUBERNETES_NAMESPACE")
             + "-" + System.getenv("DRP_CF_SERVICE") + "." + System.getenv("DRP_CF_INGRESS_HOST");
         Main.main(args);
