4ebf25037a560a5cb93924af3ebe4bb786d68296
snuyanzin <snuyanzin@gmail.com>
2020-12-16 22:23:09 +0100

    setupproperty with path to driver

--- src/main/java/net/metrosystems/mrc/CommandLineHeadlessRunner.java
+++ src/main/java/net/metrosystems/mrc/CommandLineHeadlessRunner.java
@@ -3,6 +3,8 @@ package net.metrosystems.mrc;
 import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import io.cucumber.core.cli.Main;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public final class CommandLineHeadlessRunner {
     private CommandLineHeadlessRunner() {
@@ -12,6 +14,8 @@ public final class CommandLineHeadlessRunner {
         System.setProperty("selenide.headless", "true");
         Configuration.headless = true;
         Configuration.browser = Browsers.FIREFOX;
+        Configuration.browserBinary = "drivers/geckodriver";
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
         Configuration.baseUrl = "http://" + System.getenv("DRP_CF_KUBERNETES_NAMESPACE")
             + "-" + System.getenv("DRP_CF_SERVICE") + "." + System.getenv("DRP_CF_INGRESS_HOST");
         Main.main(args);
